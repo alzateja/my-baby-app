027775111b765695eca46cfeffbb909bdaa17769
alzateja <alzateja@gmail.com>
2020-08-12 18:47:20 -0400

    Fix eslint. Make basic Login and Registration form

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const Registration = (): JSX.Element => (
-  <div>
-    <p>This is the Registration page</p>
-  </div>
+  <Form>
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Text className="text-muted">
+        We&apos;ll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+
+    <Form.Group controlId="formBasicConfirmPassword">
+      <Form.Label>Confirm Password</Form.Label>
+      <Form.Control type="password" placeholder="Confirm Password" />
+    </Form.Group>
+
+    <Button variant="primary" type="submit">
+      Register
+    </Button>
+  </Form>
 );
 
 export default Registration;
