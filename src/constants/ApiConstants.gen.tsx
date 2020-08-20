9244ad99fe978e43eec76afdbaf2cf650812769b
alzateja <alzateja@gmail.com>
2020-08-19 23:18:37 -0400

    Add ApiConstants in reason file for api calls

---
+++ src/constants/ApiConstants.gen.tsx
@@ -0,0 +1,8 @@
+/* TypeScript file generated from ApiConstants.re by genType. */
+/* eslint-disable import/first */
+
+
+// tslint:disable-next-line:no-var-requires
+const ApiConstantsBS = require('./ApiConstants.bs');
+
+export const host: string = ApiConstantsBS.host;
---
+++ src/constants/ApiConstants.re
@@ -0,0 +1,2 @@
+[@genType]
+let  host= "http://127.0.0.1:3001/";
