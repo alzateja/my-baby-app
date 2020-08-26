d147632bfe44666911d23411c1697a7d8267f8b7
alzateja <alzateja@gmail.com>
2020-08-26 18:57:22 -0400

    Add History to the Shell

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -10,7 +10,7 @@ const App = (): JSX.Element => {
 
   return (
     <AppContextProvider>
-      <Shell>
+      <Shell history={history}>
         <Router history={history}>
           <Routes />
         </Router>
